be204548440ae510920401ba7afed2a6d119df40
Ellevern <elisavandk@gmail.com>
2024-03-24 03:31:00 -0400

    Fixed size change effect when hovering over buttons. Refined stylesheet for main application.

--- src/main/java/controller/LibraryApplication.java
+++ src/main/java/controller/LibraryApplication.java
@@ -11,7 +11,7 @@ public class LibraryApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(LibraryApplication.class.getResource("/view/library-management-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 625, 625);
+        Scene scene = new Scene(fxmlLoader.load(), 625, 725);
 
         try {
             scene.getStylesheets().add(getClass().getResource("/styles/library-management.css").toExternalForm());
